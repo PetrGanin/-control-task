07100484cfca86f9c6c4894189e7154a9718597c
Petr_Ganin <stranik1985@gmail.com>
2023-02-14 10:47:22 +0300

    task 1 solution 2

---
+++ task/solution 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Создаем строковый массив. переменную count для подсчета длины второго массива, и переменную symbol для замены цифры переменной для комфортного
+// использования.
+string[] Array = { "hello", "2", ":-)", "-2", "1234" };
+Console.WriteLine(String.Join(",", Array));
+int symbol = 3;
+string [] Array2 = new string [3];
+int countArray2 = 0;
+// Цикл for для подсчета длины массива Array2.
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length <= symbol)
+    {
+         Array2[countArray2]=Array[i];
+         countArray2++;
+    }
+
+}
+// создание нового массива длиной count.
+Console.WriteLine(String.Join(",", Array2));
